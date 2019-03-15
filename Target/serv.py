29f8e43a034fc19efe279b57aac5b51e2eb89877
akulij <aakulij@gmail.com>
2019-03-15 21:37:16 +0300

    up

--- Target/serv.py
+++ Target/serv.py
@@ -5,8 +5,11 @@ import socket
 import ctypes
 import os
 import platform
+import time
+import subprocess
+port=1100
 sock = socket.socket()
-sock.bind(('', 1100))
+sock.bind(('', port))
 while True:
  sock.listen(1)
  conn, addr = sock.accept()
@@ -15,7 +18,7 @@ while True:
  add=data[:-addlen]
 #edit from this------------------------------------ x='buffalo'    
 #                                                                  exec("%s = %d" % (x,2))
-#                                                                  print buffalo
+#                                                                  print buffalo ; rfind()
  if data=='cdopen':
   os.system("eject /dev/sr0")
   conn.send('Ready')
@@ -27,12 +30,25 @@ while True:
     conn.close()
     break
  elif add=='add':
+   valpos=data.rfind('-')+1
+   valend=data[valpos:len(data)]
+   valend=int(valend)
+   pos=data.rfind('-',0,valpos-2)+1
+   space=data.rfind(' ',pos)
+   variablename=data[pos:space]
+   exec("%s = %d" % (variablename,valend))
    conn.send('this func is not ready')
- elif data=='add':
-   conn.send('syntax:add (variable)\n |enter| \n(value)\nfor example:add port |enter| 1100')
+ elif data=='reload':
+   conn.send("i can't find solution for this:(")
+ elif data=='syscmd':
+   conn.send('what to do?')
+   cmd=str(conn.recv(1024))
+   print cmd
+   conn.send('making')
+   process = subprocess.Popen("python2","test.py")
+   code = process.wait()
+   conn.send(str(code))
  else:
   conn.send('Error: Unknow command')
 #
 #to this----------------------------------------------
-
-conn.close()
